2c5ec38dc4bc702cf5115da10dfcb1d96082c760
http://jneen.net/ <jneen@jneen.net>
2015-03-10 19:04:16 -0700

    add tulip.value

---
+++ tulip/value.py
@@ -0,0 +1,35 @@
+class Value(object):
+    pass
+
+class Int(Value):
+    def __init__(self, value):
+        self.value = value
+
+class Func(Value):
+    def __init__(self, bytecode, curried, remaining):
+        self.bytecode = bytecode
+        self.curried = curried
+        self.remaining = remaining
+
+class Tagged(Value):
+    def __init__(self, symbol, args):
+        self.symbol = symbol
+        self.args = args
+
+# TODO
+class Pattern(Value):
+    def __init__(self, bytecode):
+        self.bytecode = bytecode
+
+class NativeAction(Value):
+    def __init__(self, impl):
+        self.impl = impl
+
+class NativeFunc(Value):
+    def __init__(self, arity, impl):
+        self.arity = arity
+        self.impl = impl
+
+class Lazy(Value):
+    def __init__(self, application):
+        self.application = application
